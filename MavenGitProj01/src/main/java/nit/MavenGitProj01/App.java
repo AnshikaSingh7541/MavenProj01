003e7cc9bd44e44fee62ba2a91176e3f8ada605c
AnshikaSingh <anshikasingh91996.singh@example.com>
2025-04-30 23:05:10 +0530

    comit

--- MavenGitProj01/src/main/java/nit/MavenGitProj01/App.java
+++ MavenGitProj01/src/main/java/nit/MavenGitProj01/App.java
@@ -9,9 +9,9 @@ public class App
     public static void main( String[] args )
     {
        App app=new App();
-       System.out.println(app.add(1, 2));
+      
        
-       System.out.println("good evenong ayush sir");
+       
        
        System.out.println("Anshika singh");
       
