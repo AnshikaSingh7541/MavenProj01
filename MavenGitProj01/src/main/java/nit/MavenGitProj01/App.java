6185079896eff955d8566e362d2535e8ed9d520d
AnshikaSingh <anshikasingh91996.singh@example.com>
2025-04-30 16:17:14 +0530

    commit by dev

--- MavenGitProj01/src/main/java/nit/MavenGitProj01/App.java
+++ MavenGitProj01/src/main/java/nit/MavenGitProj01/App.java
@@ -9,8 +9,8 @@ public class App
     public static void main( String[] args )
     {
        App app=new App();
-       System.out.println(app.add(1, 2));
-       
+       //System.out.println(app.add(1, 2));
+       System.out.println("good evening");
     		   
     }
 }
