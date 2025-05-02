9f119e247436c3759409d292172aecb37113e3a2
AnshikaSingh <anshikasingh91996.singh@example.com>
2025-05-02 19:20:38 +0530

    commit dv

--- MavenGitProj01/src/main/java/nit/MavenGitProj01/App.java
+++ MavenGitProj01/src/main/java/nit/MavenGitProj01/App.java
@@ -6,15 +6,15 @@ public class App
 	public int add(int a, int b) {
 		return a+b;
 	}
-	/*
-	 * public static void main( String[] args ) { App app=new App();
-	 * System.out.println("Punit sir"); System.out.println("Kodnest");
-	 * System.out.println("good evenong ayush sir");
-	 * 
-	 * 
-	 * System.out.println("Hello java");
-	 * 
-	 * 
-	 * }
-	 */
+	
+	 public static void main( String[] args ) { App app=new App();
+	  System.out.println("Punit sir"); System.out.println("Kodnest");
+	  System.out.println("good evenong ayush sir");
+	  
+	 
+	  System.out.println("Hello java");
+	  
+	  
+	  }
+	 
 }
