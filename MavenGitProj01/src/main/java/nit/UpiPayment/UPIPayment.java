685ca00145dcec239e8ca859aacb534c52acde4a
AnshikaSingh <anshikasingh91996.singh@example.com>
2025-05-02 19:21:06 +0530

    commit tl

--- MavenGitProj01/src/main/java/nit/UpiPayment/UPIPayment.java
+++ MavenGitProj01/src/main/java/nit/UpiPayment/UPIPayment.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class UPIPayment {
 
 	public String upiDone(String s,double amount) {
-		return "Transaction is done"+new Random(50000);
+		return "Transaction is done for"+amount+" "+new Random(50000);
 	}
 	
 }
